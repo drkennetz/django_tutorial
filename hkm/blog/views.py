e1d1f6a4d7c4f22fa820c0df74eb1e6e7534079e
Dennis Kennetz <drkennetz@gmail.com>
2020-01-16 07:21:42 +0000

    finished tutorial 3

--- hkm/blog/views.py
+++ hkm/blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'DennisRK',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'January 15, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'January 15, 2020'
+    }
+
+]
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
+
